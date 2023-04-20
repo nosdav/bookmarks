65c83041b8b78aad623c9e0a662eb965946e410c
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-20 21:43:37 +0200

    refactor to app.js

---
+++ app.js
@@ -0,0 +1,115 @@
+import { html, Component } from './js/spux.js';
+import { getPath, getQueryStringValue, loadFile, saveFile } from './util.js';
+
+export class App extends Component {
+  constructor() {
+    super();
+    const serverUrl = getQueryStringValue('storage') || 'https://nosdav.nostr.rocks';
+    const mode = getQueryStringValue('mode') || 'm';
+    const uri = getQueryStringValue('uri') || 'bookmarks.json';
+    this.state = {
+      userPublicKey: null,
+      filename: uri,
+      fileContent: '[]',
+      bookmarks: [],
+      newBookmarkUrl: '',
+      serverUrl: serverUrl,
+      mode: mode,
+    };
+  }
+
+  async componentDidMount() {
+    await this.userLogin();
+  }
+
+  updateNewBookmarkUrl = (event) => {
+    this.setState({ newBookmarkUrl: event.target.value });
+  }
+
+  userLogin = async () => {
+    const userPublicKey = await window.nostr.getPublicKey();
+    console.log(`Logged in with public key: ${userPublicKey}`);
+    await this.setState({ userPublicKey: userPublicKey });
+    this.loadBookmarks();
+  }
+
+  loadBookmarks = async () => {
+    const { userPublicKey, serverUrl, mode, filename } = this.state;
+    const fileContent = await loadFile(serverUrl, userPublicKey, filename, mode);
+
+    if (fileContent) {
+      this.setState({ fileContent: fileContent, bookmarks: JSON.parse(fileContent) });
+    }
+  };
+
+  saveBookmarks = async () => {
+    const { bookmarks, userPublicKey, serverUrl, mode, filename } = this.state;
+    const fileContent = JSON.stringify(bookmarks);
+    const success = await saveFile(serverUrl, userPublicKey, filename, mode, fileContent);
+
+    if (!success) {
+      alert('Error saving bookmarks');
+    }
+  };
+
+  addBookmark = () => {
+    const { newBookmarkUrl, bookmarks } = this.state;
+    if (newBookmarkUrl) {
+      const updatedBookmarks = [...bookmarks, { url: newBookmarkUrl }];
+      this.setState({ bookmarks: updatedBookmarks, newBookmarkUrl: '' }, this.saveBookmarks);
+    }
+  };
+
+  deleteBookmark = (bookmarkToDelete) => {
+    const updatedBookmarks = this.state.bookmarks.filter(
+      (bookmark) => bookmark.url !== bookmarkToDelete.url
+    );
+    this.setState({ bookmarks: updatedBookmarks }, this.saveBookmarks);
+  };
+
+  render() {
+    const { userPublicKey, newBookmarkUrl, bookmarks } = this.state;
+
+    return html`
+      <div class="container">
+        <h1>Bookmark Manager</h1>
+        <input
+          type="text"
+          id="bookmark-input"
+          placeholder="Enter a new bookmark URL"
+          value="${newBookmarkUrl}"
+          onInput="${this.updateNewBookmarkUrl}"
+        />
+
+        ${userPublicKey
+        ? html`
+              <button onClick="${this.addBookmark}" type="button">
+                Add Bookmark
+              </button>
+            `
+        : html` <button id="login" onClick="${this.userLogin}">
+              Login
+            </button>`}
+
+        <ul id="bookmark-list">
+          <hr />
+          ${bookmarks.map(
+          (bookmark) => html`
+              <li>
+                <a target="_blank" href=${bookmark.url}>${bookmark.url}</a>
+                <button
+                  onClick="${() => this.deleteBookmark(bookmark)}"
+                  type="button"
+                >
+                  Delete
+                </button>
+              </li>
+            `
+        )}
+        </ul>
+      </div>
+   
+      `;
+
+  }
+}
---
+++ util.js
@@ -0,0 +1,107 @@
+export function getPath (serverUrl, userPublicKey, filename, mode) {
+  if (isAbsoluteUrl(filename)) {
+    return filename
+  }
+
+  if (mode !== 'm') {
+    return `${serverUrl}/${filename}`
+  } else {
+    return `${serverUrl}/${userPublicKey}/${filename}`
+  }
+}
+
+export function getMimeType (filename) {
+  const extension = filename.split('.').pop().toLowerCase()
+  switch (extension) {
+    case 'txt':
+      return 'text/plain'
+    case 'ttl':
+      return 'text/turtle'
+    case 'json':
+      return 'application/json'
+    case 'jsonld':
+      return 'application/ld+json'
+    case 'md':
+      return 'text/markdown' // Added Markdown support
+    default:
+      return 'text/plain'
+  }
+}
+
+export function isAbsoluteUrl (url) {
+  try {
+    new URL(url)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+export function getQueryStringValue (key) {
+  const queryString = window.location.search.substring(1)
+  const queryParams = new URLSearchParams(queryString)
+  return queryParams.get(key)
+}
+
+export async function generateAuthorizationHeader (path) {
+  const event = {
+    kind: 27235,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [['url', path]],
+    content: ''
+  }
+  const signedEvent = await window.nostr.signEvent(event)
+  console.log(signedEvent)
+  console.log(JSON.stringify(signedEvent))
+  console.log(btoa(JSON.stringify(signedEvent)))
+
+  return `Nostr ${btoa(JSON.stringify(signedEvent))}`
+}
+
+export async function loadFile (serverUrl, userPublicKey, filename, mode) {
+  const path = getPath(serverUrl, userPublicKey, filename, mode)
+
+  try {
+    const response = await fetch(path, {
+      headers: {
+        Authorization: `Nostr ${userPublicKey}`
+      }
+    })
+
+    if (response.status === 200) {
+      return await response.text()
+    } else {
+      throw new Error('Failed to load file content')
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function saveFile (serverUrl, userPublicKey, filename, mode, fileContent) {
+  const path = getPath(serverUrl, userPublicKey, filename, mode)
+  const contentType = getMimeType(filename)
+  const authorization = await generateAuthorizationHeader(path)
+
+  try {
+    const response = await fetch(path, {
+      method: 'PUT',
+      body: fileContent,
+      headers: {
+        Authorization: authorization,
+        'Content-Type': contentType,
+        'Content-Length': fileContent.length
+      }
+    })
+
+    if (response.status === 201) {
+      console.log('File saved successfully')
+      return true
+    } else {
+      throw new Error('Failed to save file')
+    }
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
